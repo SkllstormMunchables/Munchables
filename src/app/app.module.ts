0141ffb519c6c923abcada2f535faf74698dde1d
ShaunKolich <employmeinc@gmail.com>
2019-12-03 09:50:07 -0500

    Creating new Muchables Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SliderComponent } from './slider/slider.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SliderComponent,
+    AddRecipeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
