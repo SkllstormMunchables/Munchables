1caed8411ebcc3ba06b0bb491726551dbd201b65
ShaunKolich <employmeinc@gmail.com>
2019-12-04 06:31:50 -0500

    updating slider component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { SliderComponent } from './slider/slider.component';
-import { AddRecipeComponent } from './add-recipe/add-recipe.component';
-import { FooterComponent } from './footer/footer.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { SliderComponent } from "./slider/slider.component";
+import { AddRecipeComponent } from "./add-recipe/add-recipe.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
   declarations: [
@@ -16,11 +16,8 @@ import { FooterComponent } from './footer/footer.component';
     AddRecipeComponent,
     FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
