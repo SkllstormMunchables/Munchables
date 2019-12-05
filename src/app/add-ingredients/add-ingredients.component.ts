9dcf8b2158c49ae940d015dc4a2d4115af2e76d9
ShaunKolich <employmeinc@gmail.com>
2019-12-04 21:27:36 -0500

    created the add-ingredients, add-recipe, and add-steps components

--- src/app/add-ingredients/add-ingredients.component.ts
+++ src/app/add-ingredients/add-ingredients.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { IngredientsService } from "./../ingredients.service";
+import { Ingredients } from "./../models/ingredients";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-add-ingredients',
-  templateUrl: './add-ingredients.component.html',
-  styleUrls: ['./add-ingredients.component.css']
+  selector: "app-add-ingredients",
+  templateUrl: "./add-ingredients.component.html",
+  styleUrls: ["./add-ingredients.component.css"]
 })
 export class AddIngredientsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  dummy: Ingredients = new Ingredients();
+  addIngredient(): void {
+    this.ingredientsService.add(this.dummy);
+    this.dummy = new Ingredients();
   }
 
+  constructor(private ingredientsService: IngredientsService) {}
+
+  ngOnInit() {}
 }
--- src/app/add-steps/add-steps.component.ts
+++ src/app/add-steps/add-steps.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { StepsService } from "./../steps.service";
+import { Component, OnInit } from "@angular/core";
+import { Steps } from "../models/steps";
 
 @Component({
-  selector: 'app-add-steps',
-  templateUrl: './add-steps.component.html',
-  styleUrls: ['./add-steps.component.css']
+  selector: "app-add-steps",
+  templateUrl: "./add-steps.component.html",
+  styleUrls: ["./add-steps.component.css"]
 })
 export class AddStepsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  dummy: Steps = new Steps();
+  addSteps(): void {
+    this.stepsService.add(this.dummy);
+    this.dummy = new Steps();
   }
 
+  constructor(private stepsService: StepsService) {}
+
+  ngOnInit() {}
 }
