0f9dfcf0418e5f13e8e85d813d0fa9d78af5e514
ShaunKolich <employmeinc@gmail.com>
2019-12-04 21:41:28 -0500

    Finished the list-recipe component

--- src/app/list-recipe/list-recipe.component.ts
+++ src/app/list-recipe/list-recipe.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { IngredientsService } from "./../ingredients.service";
+import { StepsService } from "./../steps.service";
+import { RecipesService } from "./../recipes.service";
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../models/recipe";
+import { Steps } from "../models/steps";
+import { Ingredients } from "../models/ingredients";
 
 @Component({
-  selector: 'app-list-recipe',
-  templateUrl: './list-recipe.component.html',
-  styleUrls: ['./list-recipe.component.css']
+  selector: "app-list-recipe",
+  templateUrl: "./list-recipe.component.html",
+  styleUrls: ["./list-recipe.component.css"]
 })
 export class ListRecipeComponent implements OnInit {
+  recipes: Recipe[] = [];
+  step: Steps[] = [];
+  ingredient: Ingredients[] = [];
 
-  constructor() { }
+  // tslint:disable-next-line: max-line-length
+  constructor(
+    private recipesService: RecipesService,
+    private stepsService: StepsService,
+    private ingredientsService: IngredientsService
+  ) {}
 
   ngOnInit() {
-  }
+    this.recipesService.list().subscribe(dummy => {
+      console.log(dummy);
+      this.recipes = dummy;
+    });
 
+    this.stepsService.list().subscribe(dummy => {
+      console.log(dummy);
+      this.step = dummy;
+    });
+
+    this.ingredientsService.list().subscribe(dummy => {
+      console.log(dummy);
+      this.ingredient = dummy;
+    });
+  }
 }
