ac7e6005d9c2290adf1117a02f96025e60a3e115
ShaunKolich <employmeinc@gmail.com>
2019-12-06 08:58:38 -0500

    removing some uncalled code in slider.ts

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,15 +1,15 @@
-import { Ingredients } from './../models/ingredients';
-import { StepsService } from './../steps.service';
-import { IngredientsService } from './../ingredients.service';
-import { RecipesService } from './../recipes.service';
-import { Recipe } from './../models/recipe';
-import { Component, OnInit } from '@angular/core';
-import { Steps } from '../models/steps';
+import { Ingredients } from "./../models/ingredients";
+import { StepsService } from "./../steps.service";
+import { IngredientsService } from "./../ingredients.service";
+import { RecipesService } from "./../recipes.service";
+import { Recipe } from "./../models/recipe";
+import { Component, OnInit } from "@angular/core";
+import { Steps } from "../models/steps";
 
 @Component({
-  selector: 'app-slider',
-  templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  selector: "app-slider",
+  templateUrl: "./slider.component.html",
+  styleUrls: ["./slider.component.css"]
 })
 export class SliderComponent implements OnInit {
   recipes: Recipe[] = [];
@@ -17,24 +17,11 @@ export class SliderComponent implements OnInit {
   ingredient: Ingredients[] = [];
 
   // tslint:disable-next-line: max-line-length
-  constructor(private recipesService: RecipesService, private ingredientsService: IngredientsService, private stepsService: StepsService) { }
+  constructor(
+    private recipesService: RecipesService,
+    private ingredientsService: IngredientsService,
+    private stepsService: StepsService
+  ) {}
 
-  ngOnInit() {
-    this.recipesService.list().subscribe(dummy => {
-      console.log(dummy);
-      this.recipes = dummy;
-    });
-
-    this.stepsService.list().subscribe(dummy => {
-      console.log(dummy);
-      this.step = dummy;
-    });
-
-    this.ingredientsService.list().subscribe(dummy => {
-      console.log(dummy);
-      this.ingredient = dummy;
-    });
-
-  }
-
+  ngOnInit() {}
 }
