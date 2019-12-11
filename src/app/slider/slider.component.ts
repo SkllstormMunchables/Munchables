12e1f0e92d2dc7c09c03df0dd6355ec512b5d94f
ShaunKolich <employmeinc@gmail.com>
2019-12-11 14:49:21 -0500

    Clearing branch

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,17 +1,19 @@
-import { RecipeIngredientsService } from './../recipe-ingredients.service';
-import { Ingredients } from './../models/ingredients';
-import { StepsService } from './../steps.service';
-import { IngredientsService } from './../ingredients.service';
-import { RecipesService } from './../recipes.service';
-import { Recipe } from './../models/recipe';
-import { Component, OnInit } from '@angular/core';
-import { Steps } from '../models/steps';
-import { RecipeIngredients} from '../models/recipeIngredients';
+import { RecipeIngredientsService } from "./../recipe-ingredients.service";
+import { Ingredients } from "./../models/ingredients";
+import { StepsService } from "./../steps.service";
+import { IngredientsService } from "./../ingredients.service";
+import { RecipesService } from "./../recipes.service";
+import { Recipe } from "./../models/recipe";
+import { Component, OnInit } from "@angular/core";
+import { Steps } from "../models/steps";
+import { RecipeIngredients } from "../models/recipeIngredients";
+
+// import 'turn.min.js';
 
 @Component({
-  selector: 'app-slider',
-  templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  selector: "app-slider",
+  templateUrl: "./slider.component.html",
+  styleUrls: ["./slider.component.css"]
 })
 export class SliderComponent implements OnInit {
   recipes: Recipe[] = [];
@@ -23,8 +25,12 @@ export class SliderComponent implements OnInit {
     private recipesService: RecipesService,
     private ingredientsService: IngredientsService,
     private stepsService: StepsService,
-    private recipeIngredientsService: RecipeIngredientsService
-  ) {}
+    private recipeIngredientsService: RecipeIngredientsService,
+
+
+  ) {
+
+  }
 
   ngOnInit() {
     this.getRecipes();
@@ -34,21 +40,29 @@ export class SliderComponent implements OnInit {
   }
 
   getRecipeIngredients(): void {
-    this.recipeIngredientsService.getRecipeIngredients().subscribe(recipeIngredient => {
-      this.recipeIngredients = recipeIngredient;
-    });
+    this.recipeIngredientsService
+      .getRecipeIngredients()
+      .subscribe(recipeIngredient => {
+        this.recipeIngredients = recipeIngredient;
+      });
   }
   genRecipeId(recipe: Recipe[]): number {
-    return recipe.length > 0 ? Math.max(...recipe.map(recipes => recipes.recipeId)) + 1 : 1;
+    return recipe.length > 0
+      ? Math.max(...recipe.map(recipes => recipes.recipeId)) + 1
+      : 1;
   }
 
   addRecipes(recipeName: string): void {
-    if (!recipeName) { return; }
-    this.recipesService.addRecipes({recipeName} as Recipe).subscribe(recipe => {
-      recipe.recipeId = this.genRecipeId(this.recipes);
-      this.recipes.push(recipe);
-      console.log(recipe);
-    });
+    if (!recipeName) {
+      return;
+    }
+    this.recipesService
+      .addRecipes({ recipeName } as Recipe)
+      .subscribe(recipe => {
+        recipe.recipeId = this.genRecipeId(this.recipes);
+        this.recipes.push(recipe);
+        console.log(recipe);
+      });
   }
 
   getRecipes(): void {
@@ -62,16 +76,20 @@ export class SliderComponent implements OnInit {
     });
   }
 
-genIngredientId(temp: Ingredients[]): number {
-    return temp.length > 0 ? Math.max(...temp.map(ingredient => ingredient.ingredientId)) + 1 : 1;
+  genIngredientId(temp: Ingredients[]): number {
+    return temp.length > 0
+      ? Math.max(...temp.map(ingredient => ingredient.ingredientId)) + 1
+      : 1;
   }
 
   addIngredient(name: string): void {
-    this.ingredientsService.addIngredient({name} as Ingredients).subscribe(temp => {
-      temp.ingredientId = this.genIngredientId(this.ingredient);
-      this.ingredient.push(temp);
-      console.log(temp);
-    });
+    this.ingredientsService
+      .addIngredient({ name } as Ingredients)
+      .subscribe(temp => {
+        temp.ingredientId = this.genIngredientId(this.ingredient);
+        this.ingredient.push(temp);
+        console.log(temp);
+      });
   }
   getIngredientById(id: number) {
     return this.ingredient.find(x => x.ingredientId === id).name;
@@ -83,8 +101,8 @@ genIngredientId(temp: Ingredients[]): number {
   }
 
   addSteps(step: string): void {
-    this.stepsService.addSteps({step} as Steps).subscribe(steps => {
+    this.stepsService.addSteps({ step } as Steps).subscribe(steps => {
       this.steps.push(steps);
     });
-}
+  }
 }
